2eaa1c186242b9b1b2209298a8d8e9ab2f8f775b
mhertogs <matthew_hertogs@yahoo.com>
2014-05-02 18:34:53 -0700

    Test Case Working
    Got a test case to run and fail!

--- GeoPostTest/src/edu/washington/geopost/test/MainActivityTest.java
+++ GeoPostTest/src/edu/washington/geopost/test/MainActivityTest.java
@@ -1,25 +1,26 @@
 package edu.washington.geopost.test;
 
-import org.junit.Test;
-
 import edu.washington.geopost.MainActivity;
-import android.app.Activity;
-import android.test.ActivityUnitTestCase;
-import android.test.AndroidTestCase;
+import android.test.ActivityInstrumentationTestCase2;
 
-public class MainActivityTest extends AndroidTestCase {
-	
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-	}
-	
-	protected void tearDown() throws Exception {
-		super.tearDown();
-	}
-	
-	public void sampleTest() {
-		System.out.println("IN sampleTest");
-		assertTrue(false);
-	}
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+	private MainActivity mFirstActivity;
+
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        MainActivity mFirstAcitivty = getActivity();
+    }
+    
+    public void testPreconditions() {
+        assertNotNull("mFirstTestActivity is null", mFirstActivity);
+    }
+    
+    public void testMyFirstTestTextView_labelText() {
+        assertEquals(1, 0);
+    }
 }
