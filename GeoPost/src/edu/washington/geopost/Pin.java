8b80ddcb726ac7438d3c717cb5191beb6090526f
mdrasnin <megandrasnin@gmail.com>
2014-05-10 11:48:55 -0700

    Added getters and setters to Pin.java and user.java.

--- GeoPost/src/edu/washington/geopost/Pin.java
+++ GeoPost/src/edu/washington/geopost/Pin.java
@@ -13,4 +13,102 @@ public class Pin {
 	private String pinId;
 	// The message that the pin will display
 	private String message;
+	
+	
+	/**
+	 * Creates a new Pin.
+	 * @param locked True if the pin is locked.
+	 * @param coord The pin's coordinates.
+	 * @param userId The userId of the pin's author.
+	 * @param pinId The pin's Id.
+	 * @param message The message that the pin will display.
+	 */
+	public Pin(boolean locked, Location coord, String userId, String pinId, String message) {
+		this.locked = locked;
+		this.coord = coord;
+		this.userId = userId;
+		this.pinId = pinId;
+		this.message = message;
+	}
+
+	/**
+	 * Returns true if the pin is locked.
+	 * @return true if the pin is locked.
+	 */
+	public boolean isLocked() {
+		return locked;
+	}
+	
+	/**
+	 * Returns the pin's Location.
+	 * @return The pin's Location.
+	 */
+	public Location getCoordinates() {
+		return coord;
+	}
+	
+	/**
+	 * Returns the userId of the pin's Author.
+	 * @return The userId of the pin's Author.
+	 */
+	public String getPinAuthor() {
+		return userId;
+	}
+	
+	/**
+	 * Returns the pin's ID.
+	 * @return The pin's ID.
+	 */
+	public String getPinId() {
+		return pinId;
+	}
+	
+	/**
+	 * Returns the message that the pin will display.
+	 * @return The message that the pin will display.
+	 */
+	public String getMessage() {
+		return message;
+	}
+	
+	
+	/**
+	 * Sets the pin's locked status.
+	 * @param locked True if the pin is locked. 
+	 */
+	public void setLocked(boolean locked) {
+		this.locked = locked;
+	}
+	
+	/**
+	 * Sets the pin's location.
+	 * @param coord The location of the pin.
+	 */
+	public void setLocation(Location coord) {
+		this.coord = coord;
+	}
+	
+	/**
+	 * Sets the author of the pin.
+	 * @param userId The author's userId.
+	 */
+	public void setPinAuthor(String userId) {
+		this.userId = userId;
+	}
+	
+	/**
+	 * Sets the pin's ID.
+	 * @param pinId The pin's ID.
+	 */
+	public void setPinId(String pinId) {
+		this.pinId = pinId;
+	}
+	
+	/**
+	 * Sets the message the pin will display.
+	 * @param message The message the pin will display.
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
 }
--- GeoPost/src/edu/washington/geopost/User.java
+++ GeoPost/src/edu/washington/geopost/User.java
@@ -7,4 +7,64 @@ public class User {
 	private int postedNum;
 	// The name of the user
 	private String name;
+	
+	/**
+	 * Creates a new User.
+	 * @param viewedNum The number of pins the user has viewed.
+	 * @param postedNum The number of pins the user has posted.
+	 * @param name The name of the user.
+	 */
+	public User(int viewedNum, int postedNum, String name) {
+		this.viewedNum = viewedNum;
+		this.postedNum = postedNum;
+		this.name = name;
+	}
+	
+	/**
+	 * Returns the number of pins the user has viewed.
+	 * @return The number of pins the user has viewed.
+	 */
+	public int getNumViewed() {
+		return viewedNum;
+	}
+	
+	/**
+	 * Returns the number of pins the user has posted.
+	 * @return The number of pins the user has posted.
+	 */
+	public int getNumPosted() {
+		return postedNum;
+	}
+	
+	/**
+	 * Returns the user's name.
+	 * @return The user's name.
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * Sets the number of pins the user has viewed.
+	 * @param viewedNum The number of pins the user has viewed.
+	 */
+	public void setNumViewed(int viewedNum) {
+		this.viewedNum = viewedNum;
+	}
+	
+	/**
+	 * Sets the number of pins the user has posted.
+	 * @param postedNum The number of pins the user has posted.
+	 */
+	public void setNumPosted(int postedNum) {
+		this.postedNum = postedNum;
+	}
+	
+	/**
+	 * Sets the user's name.
+	 * @param name The user's name.
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
 }
