2071cca2c3af08460c73717ed8a273689c17cdb5
Andrew <andrewjrepp@gmail.com>
2014-05-11 09:25:43 -0700

    Updated DBQuery's commenting and pin locking.
    Improved comments, implemented a way to see if user has previously
    viewed pin.

--- GeoPost/src/edu/washington/geopost/DBQuery.java
+++ GeoPost/src/edu/washington/geopost/DBQuery.java
@@ -1,33 +1,50 @@
 package edu.washington.geopost;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import com.parse.ParseException;
 import com.parse.ParseGeoPoint;
-import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseRelation;
 import com.parse.ParseUser;
 
 import android.location.Location;
 
+/**
+ * DBQuery is a class that retrieves information from the Parse database using
+ * parameterized queries.
+ * @authors Katie Madonna, Andrew Repp
+ */
+
 public class DBQuery {
 	/**
-	 * Returns all the pins located within the box bounded by the given corners.
-	 * @param southWest The lower left corner of the user's screen on the map
-	 * @param northEast The upper right corner of the user's screen on the map
-	 * @return A set of the pins within the given box made with passed coordinates
+	 * Returns all of the pins located within the box bounded by the given 
+	 * corners.
+	 * @param southWest The lower left corner of the box for which pins are
+	 *                  desired
+	 * @param northEast The upper right corner of the box for which pins are
+	 *                  desired
+	 * @return A set of the Pins within the given box. The set will be empty if
+	 *         no such Pins are found or there is an error fetching data.
 	 */
 	public Set<Pin> getPins(Location southWest, Location northEast) {
+		// The returned set
+		Set<Pin> pinsToDisplay = new HashSet<Pin>();
+		
+		// Convert the Locations to ParseGeoPoints
 		ParseGeoPoint sw = new ParseGeoPoint(southWest.getLatitude(), 
 											 southWest.getLongitude());
 		ParseGeoPoint ne = new ParseGeoPoint(northEast.getLatitude(),
 											 northEast.getLongitude());
+		
+		// Set up the ParseQuery for the pins within the given coordinates
 		ParseQuery<ParsePin> pinQuery = ParseQuery.getQuery("ParsePin");
 		pinQuery.whereWithinGeoBox("location", sw, ne);
 		
+		// Get the results of the query
 		List<ParsePin> queryResults = null;
 		try {
 			queryResults = pinQuery.find();
@@ -35,25 +52,40 @@ public class DBQuery {
 			System.out.println("fetching pins had an error");
 		}
 		
-		Set<Pin> pinsToDisplay = null;
-		if (queryResults != null) {
-			// TODO: Process ParsePins, convert to Pins (including setting
-			// locked status), add to Set
-			pinsToDisplay = new HashSet<Pin>();
+		if (queryResults != null) { // The query was successful
+			// Fetch the current user and set up the query for the list of pins
+			// they've viewed inside the bounding box.
+			ParseUser user = ParseUser.getCurrentUser();
+			ParseRelation<ParsePin> viewedRelation = user.getRelation("viewed");
+			ParseQuery<ParsePin> viewedQuery = viewedRelation.getQuery();
+			viewedQuery.whereWithinGeoBox("location", sw, ne);
+			
+			// Execute the query
+			List<ParsePin> viewed = new ArrayList<ParsePin>();
+			try {
+				viewed = viewedQuery.find();
+			} catch (ParseException e) { // TODO: Make this more robust
+				System.out.println("fetching viewed had an error");
+			}
+			
 			for (ParsePin pin : queryResults) {
-				boolean locked = true; // TODO: determine how to get using pin.getUser().getUsername();
+				// If this pin is in the user's list of viewed pins, then we'll
+				// mark the pin as unlocked. Otherwise, we'll set it as locked.
+				// TODO: Will this be fast enough? Also, do we want to do
+				// something special if there's an error fetching the user's
+				// viewed list?
+				boolean locked = viewed.contains(pin);
 
-				// Set up the location.
+				// Set up the pin's location.
 				// TODO: Do we need to set bearing and accuracy???
 				Location location = new Location("");
 		        location.setLatitude(pin.getLocation().getLatitude());
 		        location.setLongitude(pin.getLocation().getLongitude());
 
-				Pin newPin = new Pin(locked,
-						location,
-						pin.getUser().getUsername(),
-						pin.getObjectId(),
-						pin.getMessage());
+		        // Make the new pin and add it to the result set
+				Pin newPin = new Pin(locked, location, 
+									 pin.getUser().getUsername(),
+									 pin.getObjectId(), pin.getMessage());
 
 				pinsToDisplay.add(newPin);
 			}
@@ -75,11 +107,12 @@ public class DBQuery {
 		ParseUser user = ParseUser.getCurrentUser();
 		
 		// TODO: Connect with Facebook to get user name. We will need to set up
-		// Facebook for our app. Right now it just returns the Parse username.
+		// Facebook for our app. Right now it just returns the Parse username,
+		// and we don't know what that looks like yet.
 		name = user.getUsername();
 		
 		// Get the number of pins they've viewed
-		ParseRelation<ParseObject> viewedRelation = user.getRelation("viewed");
+		ParseRelation<ParsePin> viewedRelation = user.getRelation("viewed");
 		try {
 			viewedNum = viewedRelation.getQuery().count();
 		} catch (ParseException e) { // TODO: Make this more robust
@@ -87,7 +120,7 @@ public class DBQuery {
 		}
 		
 		// Get the number of pins they've posted
-		ParseRelation<ParseObject> postedRelation = user.getRelation("posted");
+		ParseRelation<ParsePin> postedRelation = user.getRelation("posted");
 		try {
 			postedNum = postedRelation.getQuery().count();
 		} catch (ParseException e) { // TODO: Make this more robust
