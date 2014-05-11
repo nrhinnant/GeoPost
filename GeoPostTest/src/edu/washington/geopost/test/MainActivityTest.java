10b71713903b0b068172d0f1d0b5ddf7836ad141
Ethan Goldman-Kirst <ballinethan@hotmail.com>
2014-05-10 21:12:00 -0700

    added a test

--- GeoPostTest/src/edu/washington/geopost/test/MainActivityTest.java
+++ GeoPostTest/src/edu/washington/geopost/test/MainActivityTest.java
@@ -4,21 +4,39 @@ import org.junit.Test;
 
 import edu.washington.geopost.MainActivity;
 import android.app.Activity;
+import android.content.Intent;
+import android.test.ActivityInstrumentationTestCase2;
 import android.test.ActivityUnitTestCase;
 import android.test.AndroidTestCase;
+import android.widget.Button;
 
-public class MainActivityTest extends AndroidTestCase {
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+	private MainActivity activity;
 	
+	public MainActivityTest() {
+		super(MainActivity.class);
+	}
+
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
+		activity = getActivity();
 	}
 	
 	protected void tearDown() throws Exception {
 		super.tearDown();
 	}
 	
-	public void testSampleTest() {
-		assertTrue(false);
+	public void testPreconditions() {
+		assertNotNull("activity is null", activity);
+	}
+	
+	public void testPostButtonMessage() {
+		final String expected = 
+				activity.getString(edu.washington.geopost.R.string.button_message);
+		Button postButton = (Button) activity.findViewById(edu.washington.geopost.R.id.post_button);
+		final String actual = postButton.getText().toString();
+		assertEquals(expected, actual);
 	}
 }
