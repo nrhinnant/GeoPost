78a0d05d8dd9f814b2966fa427ce50fef95fd85f
davism78 <davism78@uw.edu>
2014-05-16 11:33:13 -0700

    More logs in onMarkerClick

--- GeoPost/src/edu/washington/geopost/MainActivity.java
+++ GeoPost/src/edu/washington/geopost/MainActivity.java
@@ -195,6 +195,7 @@ public class MainActivity extends FragmentActivity
      */
 	@Override
 	public boolean onMarkerClick(Marker marker) {
+		Log.d("onMarkerClick", "marker clicked");
 		Pin pin = geoposts.get(marker);
 		if (pin == null){
 			Log.d("onMarkerClick", "clicked on marker not found in map");
@@ -208,6 +209,7 @@ public class MainActivity extends FragmentActivity
 			markerWindowShown = false;
 		} else {  // window not showing, see if we should show it
 			if (isInRange(marker) && pin.isLocked()) {
+				Log.d("onMarkerClick", "attempting to unlock in-range pin");
 				if (dbs.unlockPin(pin)) {  // unlocked new pin
 					// TODO: pin now has to be updated in the geoposts map
 					marker.showInfoWindow();
@@ -222,6 +224,7 @@ public class MainActivity extends FragmentActivity
 				markerWindowShown = true;
 				Log.d("onMarkerClick", "viewed previously unlocked pin");
 			} else {  // pin is locked
+				Log.d("onMarkerClick", "clicked on locked/out of range pin");
 				marker.hideInfoWindow();
 				markerWindowShown = false;
 				Toast toast = Toast.makeText(getApplicationContext(), "Locked", 
