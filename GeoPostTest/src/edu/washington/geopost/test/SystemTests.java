916a060dac661d0006372ac7e918505f2c2357df
Duncan26 <duncansmith1126@gmail.com>
2014-05-16 20:10:24 -0700

    Adds a (Really poorly done) system test. Sorry about this guy, I haven't really been on top of this.

--- GeoPostTest/src/edu/washington/geopost/test/SystemTests.java
+++ GeoPostTest/src/edu/washington/geopost/test/SystemTests.java
@@ -4,35 +4,59 @@ import org.junit.Test;
 
 import com.robotium.solo.Solo;
 
+import android.content.Context;
+import android.location.LocationManager;
 import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
 import edu.washington.geopost.LoginActivity;
+import edu.washington.geopost.MainActivity;
 
 public class SystemTests  extends ActivityInstrumentationTestCase2<LoginActivity>  {
 	private Solo solo;
 	String TEST_USER = "open_zjzusdi_user@tfbnw.net";
 	String TEST_USER_PASSWORD = "TestUserPass";
 	String TEST_POST_CONTENT = "TestPost from Systemtest";
+	int BIG_TIMEOUT = 20000000;
 	
-	
-	
-	public SystemTests(Class<LoginActivity> activityClass) {
-		super(activityClass);
+	public SystemTests() {
+		super(LoginActivity.class);
+	}
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
 		solo = new Solo(getInstrumentation(), getActivity());
+	}
+	
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
 	}
 
 	@Test
+	/* This test will run properly if and only if the following conditions are met
+	 * 
+	 * 		There is a user logged in to facebook
+	 * 		There is a location provider running on the phone
+	 * 
+	 * I just learned that Robotium cannot interact with the facebook login webview. 
+	 * We thus can't autologin users for facebook. This is a huge setback for me because
+	 * it means a true system test that logs into facebook etc is more or less impossible
+	 * without a more robust framework. Regardless...
+	 */
 	public void testUseCaseLogInAndDrop() {
+		solo.waitForActivity(edu.washington.geopost.LoginActivity.class);
 		solo.clickOnText("Log In");
-		solo.assertCurrentActivity("Facebook Login Screen", com.facebook.LoginActivity.class.getCanonicalName());
-		solo.clickOnEditText(0);
-		solo.enterText(0, TEST_USER);
-		solo.enterText(1, TEST_USER_PASSWORD);
-		solo.clickOnText("Continue");
-		solo.assertCurrentActivity("MainActivity", edu.washington.geopost.MainActivity.class.getCanonicalName());
-		solo.clickOnButton(0);
-		solo.enterText(0, TEST_POST_CONTENT);
-		solo.clickOnText("Post");
+		if (solo.waitForActivity(MainActivity.class, BIG_TIMEOUT)) {
+			solo.waitForText("Post");
+			solo.clickOnText("Post");
+			solo.enterText(0, TEST_POST_CONTENT);
+			solo.clickOnText("Post");
+			return;
+		}
+
 		
+		fail();
 	}
 
 }
