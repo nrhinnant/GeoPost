7a56ae34e8e5a5f693d38060744d4b24395965cf
Duncan26 <duncansmith1126@gmail.com>
2014-05-22 12:12:08 -0700

    Fixed log in state errors in LoginActivityTest

--- GeoPostTest/src/edu/washington/geopost/test/LoginActivityTest.java
+++ GeoPostTest/src/edu/washington/geopost/test/LoginActivityTest.java
@@ -1,21 +1,24 @@
 package edu.washington.geopost.test;
 
-import java.util.Collection;
-
 import android.app.Instrumentation.ActivityMonitor;
 import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
 
+import com.parse.ParseFacebookUtils;
 import com.parse.ParseUser;
+import com.robotium.solo.By;
 import com.robotium.solo.Solo;
-import com.robotium.solo.WebElement;
 
 import edu.washington.geopost.LoginActivity;
+import edu.washington.geopost.MainActivity;
 
 public class LoginActivityTest extends
 		ActivityInstrumentationTestCase2<LoginActivity> {
 
 	private Solo solo;
 	private ActivityMonitor am;
+	String TEST_USER = "open_zjzusdi_user@tfbnw.net";
+	String TEST_USER_PASSWORD = "TestUserPass";
 	
 	public LoginActivityTest() {
 		super(LoginActivity.class);
@@ -23,18 +26,28 @@ public class LoginActivityTest extends
 	
 	public void setUp() throws Exception {
 		solo = new Solo(getInstrumentation(), getActivity());
-		am = getInstrumentation().addMonitor(com.facebook.LoginActivity.class.getName(), null, false);	
+		am = getInstrumentation().addMonitor(MainActivity.class.getName(), null, false);	
+		ParseUser.logOut();
 	}
 	
 	public void testOnLoginButtonClicked() {
-		ParseUser.logOut();
 		solo.sleep(5000);
-		solo.clickOnText("Log In");
-		solo.sleep(5000);
-		
-		Collection<WebElement> we = solo.getCurrentWebElements();
-		assert(we.size() > 0);
-		
+		ParseUser currentUser = ParseUser.getCurrentUser();
+		if ((currentUser == null) || !ParseFacebookUtils.isLinked(currentUser)) {
+			// Go to the map activity
+			solo.clickOnText("Log In");
+			// Did it go to the fb login screen or the map?
+			if (getInstrumentation().waitForMonitorWithTimeout(am,  5000) == null) {
+				solo.typeTextInWebElement(By.name("email"), TEST_USER);
+				solo.typeTextInWebElement(By.name("pass"), TEST_USER_PASSWORD);
+				solo.clickOnWebElement(By.textContent("Log In"));
+				solo.sleep(5000);
+				solo.clickOnWebElement(By.textContent("OK"));
+				Log.d("geopost Systemtest", "Logged in as test user");		
+			}		
+		}
+		assertNotNull(ParseUser.getCurrentUser());
+
 		getActivity().finish();
 		ParseUser.logOut();
 	}
